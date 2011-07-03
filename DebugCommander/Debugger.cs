11e7a7f8d4909cc4b713c1ec95be0c4c40e4c930
Jason Copenhaver <jcopenha@typedef.org>
2011-07-03 14:52:21 -0400

    First winforms sample that actually works

---
+++ DebugCommander/Debugger.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace DebugCommander
+{
+    class Debugger
+    {
+        private string _Debugger;
+        private string _CommandLineArguments;
+        private string _DisplayName;
+
+        public Debugger(string DisplayName, string Debugger, string Arguments)
+        {
+            _DisplayName = DisplayName;
+            _Debugger = Debugger;
+            _CommandLineArguments = Arguments;
+
+            if (!_CommandLineArguments.Contains("%pid%"))
+                throw new ArgumentException("The command line arguments must contain a %pid% value.");
+            if (!_CommandLineArguments.Contains("%event%"))
+                throw new ArgumentException("The command line arguments must contain a %event% value.");
+        }
+
+        public bool StartDebugger(string pid, string eventnumber)
+        {
+            string commandLine = _CommandLineArguments;
+            commandLine = commandLine.Replace("%pid%", pid);
+            commandLine = commandLine.Replace("%event%", eventnumber);
+
+            ProcessStartInfo startInfo = new ProcessStartInfo(_Debugger);
+            startInfo.UseShellExecute = true;
+            startInfo.Arguments = commandLine;
+            Process p = Process.Start(startInfo);
+            
+            if (p == null)
+                return false;
+
+            // First trick.  WerFault.exe waits on the event AND the process
+            // it launches.  If the process it launches exits before the event
+            // is set then WerFault.exe thinks the crash wasn't handled and
+            // relaunchs the fault handling dialog.  So don't return from here
+            // until the debugger exits.
+            p.WaitForExit();
+            
+            return true;
+        }
+
+
+    }
+}
---
+++ DebugCommander/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace DebugCommander
+{
+    public partial class MainForm : Form
+    {
+        string _processId = "";
+        string _eventNumber = "";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            string[] args = Environment.GetCommandLineArgs();
+
+            int processIdIndex = 0;
+            int eventNumberIndex = 0;
+            for (int x = 0; x < args.Length; x++)
+            {
+                if (args[x].Equals("-p", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    processIdIndex = x+1;
+                }
+                if (args[x].Equals("-e", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    eventNumberIndex = x+1;
+                }
+            }
+
+            if (processIdIndex < args.Length)
+                _processId = args[processIdIndex];
+            if (eventNumberIndex < args.Length)
+                _eventNumber = args[eventNumberIndex];
+        }
+
+        private void btnWinDbgx86_Click(object sender, EventArgs e)
+        {
+            Debugger windbg = new Debugger("WinDbg x86",
+                                           @"C:\Program Files (x86)\Debugging Tools For Windows (x86)\WinDbg.exe",
+                                           "-p %pid% -e %event%");
+
+            this.Hide();
+
+            // this will return when the debugger exits
+            windbg.StartDebugger(_processId, _eventNumber);
+
+            Application.Exit();
+        }
+    }
+}

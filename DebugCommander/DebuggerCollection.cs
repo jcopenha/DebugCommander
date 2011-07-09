7f9e14f61a3a50ae80a8f8ea7846009e3ec6e931
Jason Copenhaver <jcopenha@typedef.org>
2011-07-08 20:15:01 -0400

    Added DebuggerCollection class to save and load the list of debuggers as well as making the GUI add the buttons based on the collection loaded.

---
+++ DebugCommander/DebuggerCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace DebugCommander
+{
+    [Serializable]
+    public class DebuggerCollection : Collection<Debugger>
+    {
+        public static DebuggerCollection Load(string fileName)
+        {
+            fileName = Path.Combine(Environment.GetFolderPath(
+                                    Environment.SpecialFolder.ApplicationData),
+                                    Path.Combine("DebugCommander", fileName));
+
+            TextReader reader = new StreamReader(fileName);
+            XmlSerializer serializer = new XmlSerializer(typeof(DebuggerCollection));
+            DebuggerCollection debuggers = (DebuggerCollection)serializer.Deserialize(reader);
+            return debuggers;
+        }
+
+        public void Save(string fileName)
+        {
+            string dir = Path.Combine(Environment.GetFolderPath(
+                                    Environment.SpecialFolder.ApplicationData),
+                                    "DebugCommander");
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+            fileName = Path.Combine(dir, fileName);
+
+            TextWriter writer = new StreamWriter(fileName);
+            XmlSerializer serializer = new XmlSerializer(typeof(DebuggerCollection));
+            serializer.Serialize(writer, this);
+        }
+    }
+}
